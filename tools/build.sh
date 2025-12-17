7e46ad9452eedd156ffd508ca19094103965f51c
Alexandre Dulaunoy <a@foo.be>
2025-12-17 22:10:31 +0100

    chg: [tools] updated build generator to use proper fonts to include emoji

--- tools/build.sh
+++ tools/build.sh
@@ -1,1 +1,5 @@
-pandoc ../book/README.md --columns=10  --pdf-engine=xelatex -V colorlinks=true -V linkcolor=blue  -V urlcolor=red  -V toccolor=gray --number-sections -V toc-own-page=true -V footnotes-pretty=true -V table-use-row-color=true --template eisvogel  -o ../output/the-art-of-pivoting.pdf  -F mermaid-filter --toc --lof
+#
+# Copy Noto Sans TTF font to your local directory - ~/.local/share/fonts/ 
+# luaotfload-tool -u
+#
+pandoc ../book/README.md --columns=10  --from markdown+emoji --pdf-engine=lualatex -V colorlinks=true -V linkcolor=blue  -V urlcolor=red  -V toccolor=gray --number-sections -V toc-own-page=true -V footnotes-pretty=true -V table-use-row-color=true --template eisvogel  -o ../output/the-art-of-pivoting.pdf  -F mermaid-filter --toc --lof
